27c6f8635fa1e76e7a7936230751b6c34243d0eb
Eveliina <pevetiainen.ep@hotmail.com>
2014-01-29 01:30:35 +0200

    VKO 3 t 3456

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,17 +9,25 @@ class RatingsController < ApplicationController
 	end
 
 	def create
-		rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-		session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-		redirect_to ratings_path
+		@rating = Rating.new params.require(:rating).permit(:score, :beer_id)
+		# session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
+		if @rating.save		
+			current_user.ratings << @rating
+			redirect_to current_user
+		else 
+			@beers = Beer.all
+			render :new
+		end
+
 	end
 	
 	def destroy
 		rating = Rating.find(params[:id])
 		rating.delete
-		redirect_to ratings_path
+		redirect_to :back
 	end
 
+	
 
 
 end	
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,13 @@
 class Brewery < ActiveRecord::Base
 
 	include RatingAverage
+
+	validates :name, presence: true
+	validates :year, numericality: { greater_than_or_equal_to: 1042,
+						#less_than_or_equal_to: 2014,
+						
+						only_integer: true}
+	validate  :year_in_future
 	
 	has_many :beers, :dependent => :destroy
 	has_many :ratings, :through => :beers
@@ -11,6 +18,15 @@ class Brewery < ActiveRecord::Base
 		puts "number of beers #{self.beers.count}"
 		puts "number of ratings #{ratings.count}"
  	end
+	
+	def year_in_future
+
+		if year.present? && year > Time.now.year
+			errors.add(:year, "can't be in the future")
+		end	
+
+	end
+
 
 	def restart
 
